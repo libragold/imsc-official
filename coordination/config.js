7c1d4691e59105b3775ac70ee11a2033d808140d
Zilin Jiang <libragold@gmail.com>
2026-07-05 07:22:21 +0800

    add coordination/config.js

---
+++ coordination/config.js
@@ -0,0 +1,5 @@
+window.COORDINATION_SUPABASE_CONFIG = {
+  url: "https://usfyecoyvmbktakoesfz.supabase.co",
+  publishableKey: "sb_publishable_WmPpTZ8Z0sqmAOCIzDEhZQ_q5OQSCQn",
+  emailDomain: "imsc-coordination.local"
+};
